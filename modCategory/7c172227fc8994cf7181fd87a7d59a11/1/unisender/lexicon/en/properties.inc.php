5fba3afc547a3c35a40b3a5a17006b5826d3f79f
grungestranger <k.shevchenko@smartcodes.ru>
2017-03-29 17:21:26 +0300

    master update 01

--- modCategory/7c172227fc8994cf7181fd87a7d59a11/1/unisender/lexicon/en/properties.inc.php
+++ modCategory/7c172227fc8994cf7181fd87a7d59a11/1/unisender/lexicon/en/properties.inc.php
@@ -5,6 +5,6 @@
  */
 $_lang['prop_unisender.ascending'] = 'Ascending';
 $_lang['prop_unisender.descending'] = 'Descending';
-$_lang['prop_unisender.list_desc'] = 'ID ������ ��� ��������';
-$_lang['prop_unisender.tags_desc'] = '���� ��� �����';
-$_lang['prop_unisender.lang_desc'] = '����. ��������������: ru,ua,en,md(hu),ge';
+$_lang['prop_unisender.list_desc'] = 'ID of the list in Unisender';
+$_lang['prop_unisender.tags_desc'] = 'Tags that are assigned to the user during the subscription from the website';
+$_lang['prop_unisender.lang_desc'] = 'Language. Supported: ru,ua,en,md(hu),ge';
